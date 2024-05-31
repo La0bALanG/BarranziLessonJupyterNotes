907e9c3b3cc78582efff179829844cf9da240654
Barranzi An <120781659@qq.com>
2024-05-31 14:54:23 +0800

    Create dmeo2.py

---
+++ section1-PythonBasic/dmeo2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+File:dmeo2.py                                         
+Time:2024/5/31 14:53                                    
+IDE:PyCharm                                     
+Author:Barranzi                                         
+email:awc19930818@outlook.com                           
+github:https://github.com/La0bALanG                     
+Barranzi's Blog:aibarranzi.com.cn                   
+requirement:(Please describle your requirement here) -->
+'''
+print('中文啊大大奥对奥对奥对奥对阿打算')
+# run test UseCase if current modules is main
+if __name__ == '__main__':
+    pass
